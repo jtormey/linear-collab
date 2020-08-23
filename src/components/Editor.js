fcb9c6b55a911d124541470663a8cf0e76627f83
Justin Tormey <jrtormey@gmail.com>
2020-08-23 10:15:03 -0500

    Mimic linear editor in overlay component

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
+import { EditorState } from 'prosemirror-state'
+import { EditorView } from 'prosemirror-view'
+import { Schema, DOMParser } from 'prosemirror-model'
+import { schema } from 'prosemirror-schema-basic'
+import { addListNodes } from 'prosemirror-schema-list'
+
+const editorSchema = new Schema({
+  nodes: addListNodes(schema.spec.nodes, 'paragraph block*', 'block'),
+  marks: schema.spec.marks
+})
+
+function scrapeEditorClasses () {
+  return document.querySelector('div[placeholder] > div').classList.value
+}
 
 export default function Editor () {
+  const elemRef = useRef(null)
+  const renderRef = useRef(true)
+
+  useEffect(() => {
+    if (elemRef.current && renderRef.current) {
+      const linearEditorElem = document.querySelector('form div[contenteditable="true"]')
+
+      window.editorView = new EditorView(elemRef.current, {
+        state: EditorState.create({
+          doc: DOMParser.fromSchema(editorSchema).parse(linearEditorElem)
+        })
+      })
+
+      renderRef.current = false
+    }
+  }, [elemRef.current])
+
   return (
-    <div>
-      hello
+    <div className={scrapeEditorClasses()}>
+      <div ref={elemRef} />
     </div>
   )
 }
--- src/components/EditorContainer.js
+++ src/components/EditorContainer.js
@@ -1,27 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
 import useElemBoundingRect from '../hooks/useElemBoundingRect'
 import Editor from './Editor'
 
+const EditorContainerStyle = styled.div`
+  position: fixed;
+  top: ${props => props.rect.y}px;
+  bottom: ${props => window.innerHeight - (props.rect.y + props.rect.height)}px;
+  left: ${props => props.rect.x}px;
+  right: ${props => window.innerWidth - (props.rect.x + props.rect.width)}px;
+  display: ${props => props.shouldDisplay ? 'block' : 'none'};
+  background-color: rgb(31, 32, 35);
+`
+
 export default function EditorContainer () {
   const rect = useElemBoundingRect('form div[contenteditable="true"]')
+  const [display, setDisplay] = useState(true)
+
+  window.setDisplay = setDisplay
 
   if (rect) {
-    const { x, y, width, height } = rect
-
-    const style = {
-      position: 'fixed',
-      top: y,
-      bottom: window.innerHeight - (y + height),
-      left: x,
-      right: window.innerWidth - (x + width),
-      background: 'black',
-      border: '2px solid red'
-    }
-
     return (
-      <div style={style}>
+      <EditorContainerStyle shouldDisplay={display} rect={rect}>
         <Editor rect={rect} />
-      </div>
+      </EditorContainerStyle>
     )
   }
 
