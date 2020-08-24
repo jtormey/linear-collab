d1bfb3a48ae287693f8697055e1d6678c3f1e02c
Justin Tormey <jrtormey@gmail.com>
2020-08-24 12:39:49 -0500

    Fix editor dom handling

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useCallback, useMemo } from 'react'
 import { createEditorDoc, mountEditorView, editorSchema } from '../prosemirror/Editor'
 import Authority from '../prosemirror/Authority'
 import SocketTransport from '../prosemirror/SocketTransport'
@@ -8,28 +8,25 @@ function scrapeEditorClasses () {
 }
 
 export default function Editor ({ issueID }) {
-  const elemRef = useRef(null)
+  const viewRef = useRef(null)
+  const transport = useMemo(() => new SocketTransport('wss://collab.linear-sync.com/ws'), [])
 
-  useEffect(() => {
-    if (elemRef.current) {
-      const linearEditorElem = document.querySelector('form div[contenteditable="true"]')
-      const editorDoc = createEditorDoc(linearEditorElem)
-      const transport = new SocketTransport('wss://collab.linear-sync.com/ws')
-      // const transport = new SocketTransport('ws://localhost:8080/ws')
-      const authority = new Authority(editorDoc, editorSchema, transport)
-
-      authority.init()
-      const view = mountEditorView(elemRef.current, authority)
-
-      return () => {
-        view.destroy()
-      }
+  const setElemRef = useCallback((elem) => {
+    if (viewRef.current) {
+      viewRef.current.destroy()
     }
-  }, [elemRef.current])
+
+    const linearEditorElem = document.querySelector('form div[contenteditable="true"]')
+    const editorDoc = createEditorDoc(linearEditorElem)
+    const authority = new Authority(editorDoc, editorSchema, transport)
+
+    authority.init()
+    viewRef.current = mountEditorView(elem, authority)
+  }, [])
 
   return (
     <div className={scrapeEditorClasses()}>
-      <div ref={elemRef} />
+      <div ref={setElemRef} />
     </div>
   )
 }
